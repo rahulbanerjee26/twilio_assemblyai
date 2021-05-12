871c7ac5d40a0e226041be1ec13ac5b0eac4b156
Rahul Banerjee <40443639+rahulbanerjee26@users.noreply.github.com>
2021-05-11 22:41:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-import requests
+'''
+    Script to send recording url to
+    AssemblyAI and poll for status
+'''
+
 import os
 from dotenv import load_dotenv
 from transcribe import get_transcribe_id, get_text
+
 
 # Reading Data from .env file
 load_dotenv()
 account_sid = os.environ.get('account_sid')
 recording_sid = os.environ.get('recording_sid')
 assemblyai_token = os.environ.get('assemblyai_token')
-recording_endpoint = f'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Recordings/{recording_sid}.mp3'
+recording_endpoint = 'https://api.twilio.com/2010-04-01/Accounts/' \
++ f'{account_sid}/Recordings/{recording_sid}.mp3'
 
 transcribe_id = get_transcribe_id(assemblyai_token,recording_endpoint)
 print(f"Transcription ID is {transcribe_id}")
-result = {}
 
+result = {}
 print("AssemblyAI is processing the file")
 while result.get("status") != 'completed':
-            result = get_text(assemblyai_token,transcribe_id)
+    result = get_text(assemblyai_token,transcribe_id)
 
 print("Transcription Complete - The result is below")
 print(result['text'])
